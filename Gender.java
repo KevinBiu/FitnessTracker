d1d7025f986cef7088448febed9f3bfcc04b7031
LiveOkay <kevin.bui709@gmail.com>
2019-06-08 14:55:32 -0400

    ok

--- Gender.java
+++ Gender.java
@@ -3,9 +3,18 @@ package FitnessGUI;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.awt.Color;
+import javax.swing.JButton;
 
 public class Gender extends JFrame {
 
@@ -32,11 +41,70 @@ public class Gender extends JFrame {
 	 */
 	public Gender() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 843, 578);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		JPanel Ovpanel = new JPanel();
+		Ovpanel.setBackground(Color.BLACK);
+		Ovpanel.setBounds(0, 0, 825, 531);
+		contentPane.add(Ovpanel);
+		Ovpanel.setBackground(new Color(0,0,0,200));
+		Ovpanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("MALE");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 27));
+		lblNewLabel.setBounds(209, 110, 82, 64);
+		Ovpanel.add(lblNewLabel);
+		
+		JLabel lblFemale = new JLabel("FEMALE");
+		lblFemale.setForeground(Color.WHITE);
+		lblFemale.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 27));
+		lblFemale.setBounds(530, 101, 109, 82);
+		Ovpanel.add(lblFemale);
+		
+		JButton MaleButton = new JButton("");
+		MaleButton.setBackground(Color.LIGHT_GRAY);
+		MaleButton.setForeground(Color.WHITE);
+		MaleButton.setBounds(133, 187, 218, 215);
+		Ovpanel.add(MaleButton);
+		
+		
+		try {
+			Image Mimg = ImageIO.read(getClass().getResource("/male-512.png"));
+			Image newimg = Mimg.getScaledInstance(MaleButton.getWidth(), MaleButton.getHeight(), java.awt.Image.SCALE_SMOOTH);
+			MaleButton.setIcon(new ImageIcon(newimg));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		JButton Febutton = new JButton("");
+		Febutton.setBackground(Color.LIGHT_GRAY);
+		Febutton.setBounds(469, 187, 218, 215);
+		Ovpanel.add(Febutton);
+		
+		
+		try {
+			Image Fimg = ImageIO.read(getClass().getResource("/female-512.png"));
+			Image newimg = Fimg.getScaledInstance(Febutton.getWidth(), Febutton.getHeight(), java.awt.Image.SCALE_SMOOTH);
+			Febutton.setIcon(new ImageIcon(newimg));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		JLabel BaLabel = new JLabel("New label");
+		BaLabel.setBounds(0, 0, 825, 531);
+		contentPane.add(BaLabel);
+		ImageIcon img = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/workout2.jpg")));
+		
+		Image img2 = img.getImage();
+		Image dimg = img2.getScaledInstance(BaLabel.getWidth(), BaLabel.getHeight(), Image.SCALE_SMOOTH);
+		
+		ImageIcon i = new ImageIcon(dimg);
+		BaLabel.setIcon(i);
 	}
-
 }
--- Start.java
+++ Start.java
@@ -2,10 +2,22 @@ package FitnessGUI;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Color;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.JSeparator;
+import javax.swing.JButton;
 
 public class Start extends JFrame {
 
@@ -32,11 +44,48 @@ public class Start extends JFrame {
 	 */
 	public Start() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 843, 609);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel Overpanel = new JPanel();
+		Overpanel.setBackground(Color.BLACK);
+		Overpanel.setBounds(0, 0, 825, 562);
+		contentPane.add(Overpanel);
+		Overpanel.setBackground(new Color(0,0,0,200));
+		Overpanel.setLayout(null);
+		
+		JLabel lblFitnessTracker = new JLabel("Fitness Tracker 5000");
+		lblFitnessTracker.setForeground(Color.WHITE);
+		lblFitnessTracker.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 46));
+		lblFitnessTracker.setBounds(195, 94, 474, 117);
+		Overpanel.add(lblFitnessTracker);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(182, 199, 487, 15);
+		Overpanel.add(separator);
+		
+		JButton SButton = new JButton("START");
+		SButton.setFont(new Font("Yu Gothic UI Semilight", Font.PLAIN, 24));
+		SButton.setForeground(Color.WHITE);
+		SButton.setBackground(Color.LIGHT_GRAY);
+		SButton.setBounds(324, 294, 172, 56);
+		Overpanel.add(SButton);
+		
+		JLabel BLabel = new JLabel("New label");
+		BLabel.setBackground(new Color(240, 240, 240));
+		BLabel.setBounds(0, 0, 825, 562);
+		contentPane.add(BLabel);
+		
+		ImageIcon img = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/workout.jpg")));
+		
+		Image img2 = img.getImage();
+		Image dimg = img2.getScaledInstance(BLabel.getWidth(), BLabel.getHeight(), Image.SCALE_SMOOTH);
+		
+		ImageIcon i = new ImageIcon(dimg);
+		BLabel.setIcon(i);
+		
 	}
-
 }
